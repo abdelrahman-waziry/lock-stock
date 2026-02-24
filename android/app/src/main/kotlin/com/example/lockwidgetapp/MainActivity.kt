2c5c18da7477e2ccaf9f22b4f01c83f4101ec0ea
abdelrahman-waziry <abdelrahman.waziry@gmail.com>
2026-02-24 04:12:55 +0200

    update

--- android/app/src/main/kotlin/com/example/lockwidgetapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/lockwidgetapp/MainActivity.kt
@@ -5,11 +5,11 @@ import android.app.admin.DevicePolicyManager
 import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
 
     private val channelName = "com.example.lockwidgetapp/lock"
     private lateinit var dpm: DevicePolicyManager
@@ -64,4 +64,3 @@ class MainActivity : FlutterActivity() {
                 }
             }
     }
-}
